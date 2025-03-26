84212cc45ec0f2c0a14e86027eb39dc291f29fea
hossein zare <hosseiny625@gmail.com>
2025-03-26 18:37:00 +0330

    chore: implement Application class

---
+++ app/src/main/java/ir/romina/hossein/core/StationApplication.kt
@@ -0,0 +1,18 @@
+package ir.romina.hossein.core
+
+import android.app.Application
+import ir.romina.hossein.core.di.networkModule
+import ir.romina.hossein.features.map.di.mapModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.GlobalContext.startKoin
+
+class StationApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@StationApplication)
+            modules(networkModule, mapModule)
+        }
+    }
+}
